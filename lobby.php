451d5b0c919c1a414bc599a10aa1074bdfa8557f
leguigue <guigs13@outlook.fr>
2024-07-28 16:17:22 +0200

    lobby.php

--- lobby.php
+++ lobby.php
@@ -52,7 +52,7 @@ function validatePassword() {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
     if (password !== confirmPassword) {
-        alert("Les mots de passe ne correspondent pas.");
+        console.log("Les mots de passe ne correspondent pas.");
         return false;
     }
     return true;
